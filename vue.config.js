42e5f097b8f2e5b5afb50d075ea65f3b059a539b
bobi <bobi1234@foxmail.com>
2019-09-06 09:02:26 +0800

    add:vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    devServer: {
+        // 自动打开浏览器
+        open: true,
+        // 设置为0.0.0.0则所有的地址均能访问
+        host: '0.0.0.0',
+        // 启动端口
+        port: 8000
+    },
+}
