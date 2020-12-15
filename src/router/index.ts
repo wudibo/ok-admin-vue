a65239a46abfdd37e2d0fccbae651448218f5294
hello <zhizous@163.com>
2020-12-15 18:37:49 +0800

    :construction: tabBar

--- src/router/index.ts
+++ src/router/index.ts
@@ -81,6 +81,183 @@ export const asyncRoutes: Array<RouteRecordRaw> = [
                         meta: {title: '测试3-2'},
                     }
                 ]
+            },
+            {
+                path: 'list-form4',
+                component: () => import('@/views/form/form-temp.vue'),
+                name: 'listForm4',
+                meta: {title: '测试4'},
+                children: [
+                    {
+                        path: 'list-form-4-1',
+                        component: () => import('@/views/form/list-form.vue'),
+                        name: 'listForm4-1',
+                        meta: {title: '测试4-1'},
+                    },
+                    {
+                        path: 'list-form-4-2',
+                        component: () => import('@/views/form/list-form.vue'),
+                        name: 'listForm4-2',
+                        meta: {title: '测试4-2'},
+                    }
+                ]
+            },
+            {
+                path: 'list-form5',
+                component: () => import('@/views/form/form-temp.vue'),
+                name: 'listForm5',
+                meta: {title: '测试5'},
+                children: [
+                    {
+                        path: 'list-form-5-1',
+                        component: () => import('@/views/form/list-form.vue'),
+                        name: 'listForm5-1',
+                        meta: {title: '测试5-1'},
+                    },
+                    {
+                        path: 'list-form-5-2',
+                        component: () => import('@/views/form/list-form.vue'),
+                        name: 'listForm5-2',
+                        meta: {title: '测试5-2'},
+                    },
+                    {
+                        path: 'list-form-5-3',
+                        component: () => import('@/views/form/form-temp.vue'),
+                        name: 'listForm5-3',
+                        meta: {title: '测试5-3'},
+                        children: [
+                            {
+                                path: 'list-form-5-3-1',
+                                component: () => import('@/views/form/list-form.vue'),
+                                name: 'listForm5-3-1',
+                                meta: {title: '测试5-3-1'},
+                            },
+                            {
+                                path: 'list-form-5-3-2',
+                                component: () => import('@/views/form/list-form.vue'),
+                                name: 'listForm5-3-2',
+                                meta: {title: '测试5-3-2'},
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        path: '/user',
+        component: (Layout as any),
+        name: 'user',
+        meta: {
+            title: '用户',
+            icon: 'dashboard'
+        },
+        children: [
+            {
+                path: 'advanced-form',
+                component: () => import('@/views/form/advanced-form.vue'),
+                name: 'advancedForm',
+                meta: {title: '测试1'},
+            },
+            {
+                path: 'list-form2',
+                component: () => import('@/views/form/form-temp.vue'),
+                name: 'listForm2',
+                meta: {title: '测试2'},
+                children: [
+                    {
+                        path: 'list-form2-1',
+                        component: () => import('@/views/form/list-form.vue'),
+                        name: 'listForm2-1',
+                        meta: {title: '测试2-1'},
+                    },
+                    {
+                        path: 'list-form2-2',
+                        component: () => import('@/views/form/list-form.vue'),
+                        name: 'listForm2-2',
+                        meta: {title: '测试2-2'},
+                    }
+                ]
+            },
+            {
+                path: 'list-form3',
+                component: () => import('@/views/form/form-temp.vue'),
+                name: 'listForm3',
+                meta: {title: '测试3'},
+                children: [
+                    {
+                        path: 'list-form-3-1',
+                        component: () => import('@/views/form/list-form.vue'),
+                        name: 'listForm3-1',
+                        meta: {title: '测试3-1'},
+                    },
+                    {
+                        path: 'list-form-3-2',
+                        component: () => import('@/views/form/list-form.vue'),
+                        name: 'listForm3-2',
+                        meta: {title: '测试3-2'},
+                    }
+                ]
+            },
+            {
+                path: 'list-form4',
+                component: () => import('@/views/form/form-temp.vue'),
+                name: 'listForm4',
+                meta: {title: '测试4'},
+                children: [
+                    {
+                        path: 'list-form-4-1',
+                        component: () => import('@/views/form/list-form.vue'),
+                        name: 'listForm4-1',
+                        meta: {title: '测试4-1'},
+                    },
+                    {
+                        path: 'list-form-4-2',
+                        component: () => import('@/views/form/list-form.vue'),
+                        name: 'listForm4-2',
+                        meta: {title: '测试4-2'},
+                    }
+                ]
+            },
+            {
+                path: 'list-form5',
+                component: () => import('@/views/form/form-temp.vue'),
+                name: 'listForm5',
+                meta: {title: '测试5'},
+                children: [
+                    {
+                        path: 'list-form-5-1',
+                        component: () => import('@/views/form/list-form.vue'),
+                        name: 'listForm5-1',
+                        meta: {title: '测试5-1'},
+                    },
+                    {
+                        path: 'list-form-5-2',
+                        component: () => import('@/views/form/list-form.vue'),
+                        name: 'listForm5-2',
+                        meta: {title: '测试5-2'},
+                    },
+                    {
+                        path: 'list-form-5-3',
+                        component: () => import('@/views/form/form-temp.vue'),
+                        name: 'listForm5-3',
+                        meta: {title: '测试5-3'},
+                        children: [
+                            {
+                                path: 'list-form-5-3-1',
+                                component: () => import('@/views/form/list-form.vue'),
+                                name: 'listForm5-3-1',
+                                meta: {title: '测试5-3-1'},
+                            },
+                            {
+                                path: 'list-form-5-3-2',
+                                component: () => import('@/views/form/list-form.vue'),
+                                name: 'listForm5-3-2',
+                                meta: {title: '测试5-3-2'},
+                            }
+                        ]
+                    }
+                ]
             }
         ]
     }
--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,37 +1,125 @@
 <template>
-  <div class="page">
-    <div class="cont">
-      <a-button @click="cont++">
-        click {{cont}}
-      </a-button>
-    </div>
+  <div style="width: 256px">
+    <a-button type="primary" @click="toggleCollapsed" style="margin-bottom: 16px">
+      <MenuUnfoldOutlined v-if="collapsed"/>
+      <MenuFoldOutlined v-else/>
+    </a-button>
+    <a-button @click="headerSelect" style="margin-left: 20px">阿萨德路附近了</a-button>
+    <a-menu
+        v-model:selected-keys="selectedKeys"
+        v-model:open-keys="openKeys"
+        mode="inline"
+        theme="dark"
+        :inline-collapsed="collapsed"
+    >
+      <template v-for="item in list" :key="item.key">
+        <template v-if="!item.children">
+          <a-menu-item :key="item.key">
+            <PieChartOutlined/>
+            <span>{{ item.title }}</span>
+          </a-menu-item>
+        </template>
+        <template v-else>
+          <sub-menu :menu-info="item" :key="item.key"/>
+        </template>
+      </template>
+    </a-menu>
   </div>
 </template>
 
-<script lang="ts">
-import {ref, onMounted} from 'vue'
+<script>
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  PieChartOutlined,
+  MailOutlined
+} from '@ant-design/icons-vue';
+import {Menu} from 'ant-design-vue';
 
+// you can rewrite it to a single file component, if not, you should config vue alias to vue/dist/vue.esm-bundler.js
+const SubMenu = {
+  components: {
+    PieChartOutlined,
+    MailOutlined,
+  },
+  template: `
+    <a-sub-menu :key="menuInfo.key" v-bind="$attrs">
+    <template #title>
+        <span>
+          <MailOutlined/><span>{{ menuInfo.title }}</span>
+        </span>
+    </template>
+    <template v-for="item in menuInfo.children" :key="item.key">
+      <template v-if="!item.children">
+        <a-menu-item :key="item.key">
+          <PieChartOutlined/>
+          <span>{{ item.title }}</span>
+        </a-menu-item>
+      </template>
+      <template v-else>
+        <sub-menu :menu-info="item" :key="item.key"/>
+      </template>
+    </template>
+    </a-sub-menu>
+  `,
+  name: 'SubMenu',
+  props: {
+    menuInfo: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+};
 export default {
-  setup() {
-    const collapsed = ref(false),
-        cont = ref(0);
-    onMounted(function (){
-      console.log('onMounted');
-    })
+  components: {
+    'sub-menu': SubMenu,
+    MenuFoldOutlined,
+    MenuUnfoldOutlined,
+    PieChartOutlined,
+  },
+  data() {
     return {
-      cont,
-      collapsed
-    }
-  }
-}
+      collapsed: false,
+      openKeys: ['2', '2.1'],
+      selectedKeys: ['2.1.1'],
+      list: [
+        {
+          key: '1',
+          title: 'Option 1',
+        },
+        {
+          key: 'a',
+          title: 'Option a',
+        },
+        {
+          key: 'b',
+          title: 'Option b',
+        },
+        {
+          key: '2',
+          title: 'Navigation 2',
+          children: [
+            {
+              key: '2.1',
+              title: 'Navigation 3',
+              children: [
+                {key: '2.1.1', title: 'Option 2.1.1'},
+                {key: '2.1.2', title: 'Option 2.1.2'}
+              ],
+            },
+          ],
+        },
+      ],
+    };
+  },
+  methods: {
+    headerSelect() {
+      // this.openKeys = ['2', '2.1'];
+      this.selectedKeys = this.selectedKeys[0] === '2.1.2' ? ['2.1.1'] : ['2.1.2'];
+    },
+    toggleCollapsed() {
+      this.collapsed = !this.collapsed;
+    },
+  },
+};
 </script>
-
-<style lang="less" scoped>
-.cont {
-  padding: 20px;
-}
-
-.flex {
-  display: flex;
-}
-</style>
