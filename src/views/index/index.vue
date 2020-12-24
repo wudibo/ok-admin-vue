d2ca136219b59ec64989f40f29d6a849b7c00e6d
hello <zhizous@163.com>
2020-12-24 13:48:40 +0800

    :tangerine: proxy

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script lang='ts'>
-import {onMounted, ref, reactive, SetupContext ,getCurrentInstance} from 'vue'
+import {onMounted, ref, reactive, SetupContext, getCurrentInstance} from 'vue'
 import Cards from "@/views/index/components/Cards.vue";
 interface Data {
   [key: string]: unknown;
@@ -13,18 +13,14 @@ interface Data {
 export default {
   components: {Cards},
   setup(props: Data, context: SetupContext) {
-    const instance = getCurrentInstance(),
+    const {proxy} = getCurrentInstance() as any,
      readersNumber = ref(0),
      book = reactive({
       title: 'Vue 3 Guide',
       food: 'bar'
     })
-
     onMounted(() => {
-      if(instance){
-        const {$http} = instance.appContext.config.globalProperties;
-        console.log($http);
-      }
+      console.log(proxy.$http);
     });
 
     return {
