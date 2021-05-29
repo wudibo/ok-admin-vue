1a7927c3a9d013c895f6a0627837f30431f5f416
jianglong <zhizous@163.com>
2021-05-29 11:00:36 +0800

    :tada: vuetify3.0

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,48 @@
 <template>
   <v-app>
-    <v-main>
-      <HelloWorld/>
+    <v-navigation-drawer permanent>
+
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-toolbar dark
+                 prominent>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      </v-toolbar>
+
+      <v-app-bar-title>Vuetify</v-app-bar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn text>Developer</v-btn>
+      <v-btn class="mr-3" text>Help</v-btn>
+      <v-divider inset vertical></v-divider>
+      <v-btn class="mr-4 ml-5"
+          color="primary"
+          plain >
+        <v-icon left icon="mdi-handshake-outline"></v-icon>
+        <span>John Leider</span>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main><!-- 给应用提供合适的间距 -->
+      <v-container fluid><!-- 如果使用 vue-router -->
+        <lay-main></lay-main>
+<!--        <router-view></router-view>-->
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import {defineComponent} from 'vue';
+import LayMain from "./layout/LayMain.vue";
 
-export default {
+export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld,
-  },
+  components: {LayMain},
   setup() {
     return {}
   }
-}
+})
 </script>
---
+++ src/layout/LayMain.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="foo"> {{ foo }}</div>
+  <div> {{ bar }}</div>
+</template>
+<script setup lang="ts">
+import {ref} from 'vue';
+
+const foo = ref('hello world');
+const bar = ref('bar');
+
+</script>
+<style lang="scss" scoped>
+.foo {
+
+}
+</style>
