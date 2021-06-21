dee6dd9226569cf034db58d13af1b30d7ecce1a3
hello <zhizous@163.com>
2021-06-21 15:39:39 +0000

    删除文件 src

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import vuetify from './plugins/vuetify'
-import App from './App.vue'
-
-console.log("Main.ts")
-const app = createApp(App)
-app.use(vuetify)
-
-app.mount('#app')
