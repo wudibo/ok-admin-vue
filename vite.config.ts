89d89fc951734fdca213625089461ba614e67e14
jianglong <zhizous@163.com>
2021-06-23 12:35:44 +0800

    :memo: 配置

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,12 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path';
 
 // https://vitejs.dev/config/
+// https://www.vitejs.net/guide/build.html#公共基础路径
 // Dotenv 是一个零依赖的模块，它能将环境变量中的变量从 .env 文件加载到 process.env 中
 // const dotenv = require("dotenv")
 export default defineConfig({
   plugins: [vue()],
-  base: "ok-admin-vue",
+  // base: "ok-admin-vue",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
