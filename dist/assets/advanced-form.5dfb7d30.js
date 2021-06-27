cca7fd0d924e1e7d377610f670f0f1dd3099283f
hello <zhizous@163.com>
2021-06-27 22:26:02 +0800

    :package: 打包

--- dist/assets/advanced-form.5dfb7d30.js
+++
@@ -1,1 +0,0 @@
-import{d as r,o as a,c as d}from"./vendor.51f221d9.js";var e=r({name:"advanced-form"});e.render=function(r,e,n,o,f,m){return a(),d("div",null,"advanced-form")};export default e;
--- dist/assets/basic-form.12e0a9ae.js
+++
@@ -1,1 +0,0 @@
-import{d as r,o as a,c as o}from"./vendor.51f221d9.js";var e=r({name:"basic-form"});e.render=function(r,e,n,d,f,s){return a(),o("div",null,"basic-form")};export default e;
