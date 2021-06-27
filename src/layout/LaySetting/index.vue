15949f14d9541256a7ad2d9a3e4076cbda457bf3
hello <zhizous@163.com>
2021-06-27 09:06:46 +0800

    icon

---
+++ src/layout/LaySetting/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>设置</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "LaySetting"
+});
+</script>
+
+<style></style>
