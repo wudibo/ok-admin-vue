0523e754f9a3d95e25d6fbdcd238f60ad20f15d8
hello <zhizous@163.com>
2021-06-28 23:15:50 +0800

    exception: 异常页面

---
+++ src/views/exception/500.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>500</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  nmae: "500"
+});
+</script>
+
+<style lang="scss"></style>
