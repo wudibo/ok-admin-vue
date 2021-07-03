3d761ab1de0332352efa0aacbe0d3c0c6c8f65c9
jianglong <zhizous@163.com>
2021-07-03 11:25:49 +0800

    :heavy_plus_sign: naiveui常用的全局组件

---
+++ src/naiveui/index.ts
@@ -0,0 +1,10 @@
+import { App } from 'vue';
+import { NButton, NIcon } from 'naive-ui';
+
+// 这里添加经常使用的全局组件
+export default {
+  install(_app: App) {
+    _app.component('NButton', NButton);
+    _app.component('NIcon', NIcon);
+  },
+};
