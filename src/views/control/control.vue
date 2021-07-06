78e9c2038aaac3b66823117c16f5cbbf057094c3
jianglong <zhizous@163.com>
2021-07-06 18:07:32 +0800

    add: 控制台

---
+++ src/views/control/control.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="box">{{ hello }} - {{ foo }}</div>
+    <n-button @click="headerKeep">foo</n-button>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue';
+const hello = ref('控制台');
+const foo = ref('bar');
+const headerKeep = () => {
+  foo.value = 'dsafasd' + Math.random() * 100;
+};
+</script>
+<style lang="scss" scoped>
+.box {
+  color: #333333;
+}
+</style>
