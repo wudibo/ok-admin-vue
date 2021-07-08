001dfd00261cbb5e1645080ba330e6f0b5493404
jianglong <zhizous@163.com>
2021-07-08 14:24:31 +0800

    :package: 打包

---
+++ dist/assets/comps.b4f857fd.js
@@ -0,0 +1,1 @@
+import{j as a,x as s,y as o,o as e,c as l,B as t,A as d}from"./vendor.40539220.js";var r={setup:()=>({hello:a("hello world")})};const c=d();s("data-v-448a2723");const n={class:"box"};o();const v=c(((a,s,o,d,r,c)=>(e(),l("div",n,t(d.hello),1))));r.render=v,r.__scopeId="data-v-448a2723";export default r;
