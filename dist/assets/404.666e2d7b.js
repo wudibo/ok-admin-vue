70ccbd7a7d313faccbf7a2dfadd2485aa5dfcc3e
jianglong <zhizous@163.com>
2021-07-12 18:48:16 +0800

    :package: 打包

---
+++ dist/assets/404.666e2d7b.js
@@ -0,0 +1,1 @@
+import{d as r,o as a,a as e}from"./vendor.362922a5.js";var n=r({nmae:"404"});n.render=function(r,n,o,d,t,s){return a(),e("div",null,"404")};export default n;
---
+++ dist/assets/500.dc4fadb3.js
@@ -0,0 +1,1 @@
+import{d as r,o as a,a as e}from"./vendor.362922a5.js";var n=r({nmae:"500"});n.render=function(r,n,o,d,t,s){return a(),e("div",null,"500")};export default n;
