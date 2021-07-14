ae5037045aa282165e7f489c2b4f2b937eeb1d4d
hello <zhizous@163.com>
2021-07-14 22:15:39 +0800

    add: CardLink.vue

--- src/views/home/components/CardLink.vue
+++ src/views/home/components/CardLink.vue
@@ -9,7 +9,7 @@
       </div>
     </n-gi>
     <n-gi v-for="(item, key) in appThemeList" :key="key">
-      <div class="bg-white link-cont" :style="{ olor: item.primaryColor }">
+      <div class="bg-white link-cont" :style="{ color: item.primaryColor }">
         <n-icon size="24">
           <logo-octocat />
         </n-icon>
