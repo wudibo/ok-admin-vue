6d777ec86e39fa5b1623b659129d71d265909b57
hello <zhizous@163.com>
2021-07-14 22:14:06 +0800

    add: home首页

--- src/layout/utils/componentParcel.ts
+++ src/layout/utils/componentParcel.ts
@@ -5,7 +5,7 @@ import store from '@/store/store.ts';
  * 功能如下
  * 1.处理transition过渡组件无法嵌套多层元素的问题
  * 2.处理没有给组件指定name值导致页面keepAlive缓存问题
- * 3.主题样式 --primary-color,--primary-colorHover,--primary-colorPressed
+ * 3.主题样式 --primary-color,--primary-color-hover,--primary-color-pressed
  * @returns
  */
 export default function (component: any) {
--- src/views/home/components/CardLink.vue
+++ src/views/home/components/CardLink.vue
@@ -1,7 +1,21 @@
 <template>
   <n-grid x-gap="12" y-gap="12" cols="8 xs:2 s:4 m:4" responsive="screen">
+    <n-gi>
+      <div class="bg-white link-cont">
+        <n-icon size="24">
+          <logo-octocat />
+        </n-icon>
+        <div class="text">主题色</div>
+      </div>
+    </n-gi>
     <n-gi v-for="(item, key) in appThemeList" :key="key">
-      <div class="bg-white link-cont" :style="{ color: item.primaryColor }">
+      <div
+        class="bg-white link-cont"
+        :style="{
+          color: item.primaryColor,
+          hover: `{ boxShadow: 0 1px 6px ${item.primaryColor} }`
+        }"
+      >
         <n-icon size="24">
           <logo-octocat />
         </n-icon>
@@ -19,6 +33,11 @@
 </script>
 
 <style lang="scss" scoped>
+  @mixin boxShadow($radius: 0, $bg: white, $shadow-color: rgba(0, 0, 0, 0.2)) {
+    background-color: $bg;
+    border-radius: $radius + 0px;
+    box-shadow: 0 1px 6px $shadow-color;
+  }
   .text {
     text-transform: uppercase;
     padding-top: 5px;
@@ -31,10 +50,11 @@
     transition: all 0.2s;
     border-radius: 4px;
     padding: 16px;
-
+    color: var(--primary-color-hover);
     &:hover {
-      border-color: #eee;
-      // .boxShadow();
+      border-color: var(--primary-color-hover);
+      transition: all 0.25s;
+      box-shadow: 0 1px 6px var(--primary-color-hover);
     }
   }
 </style>
