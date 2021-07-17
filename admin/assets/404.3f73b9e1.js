0fa43c0b4143aabe3379be88f64a6b66fe8d8d53
hello <zhizous@163.com>
2021-07-17 19:45:23 +0800

    :package: 打包

---
+++ admin/assets/404.3f73b9e1.js
@@ -0,0 +1,1 @@
+import{d as e,e as r,f as a}from"./vendor.e7580893.js";var n=e({nmae:"404"});const o={class:"app-container"};n.render=function(e,n,s,t,d,c){return r(),a("div",o,"404")};export default n;
---
+++ admin/assets/basic-form.f7ea10d8.js
@@ -0,0 +1,1 @@
+import{d as r,e as a,f as e}from"./vendor.e7580893.js";var n=r({name:"basic-form"});const o={class:"app-container"};n.render=function(r,n,s,t,c,f){return a(),e("div",o,"basic-form")};export default n;
