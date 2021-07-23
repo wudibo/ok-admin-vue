557264ff798b533a432918f0336324571111308c
jianglong <zhizous@163.com>
2021-07-23 16:20:08 +0800

    :package: 打包

--- admin/assets/404.b9897bd0.js
+++
@@ -1,1 +0,0 @@
-import{d as a,e as r,f as e}from"./vendor.838173a7.js";var n=a({nmae:"404"});const o={class:"app-container"};n.render=function(a,n,s,t,d,c){return r(),e("div",o,"404")};export default n;
