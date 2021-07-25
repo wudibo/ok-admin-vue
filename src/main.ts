14936fd010122d3e13bedd09bbc08a5c0def0613
hello <zhizous@163.com>
2021-07-25 16:25:04 +0800

    :memo: 全局自定义组件

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,16 @@ import App from './App.vue';
 import router from './router/router';
 import store from './store/store';
 import NaiveUi from './naiveui/index';
+import globalComponents from './components/global.ts';
 
 import './assets/css/app.scss';
 
 const app = createApp(App);
-// 全局NaiveUI组件
+// 注册全局NaiveUI组件
 app.use(NaiveUi);
+
+// 注册全局自定义组件
+app.use(globalComponents);
 
 // 挂载vuex状态管理
 app.use(store);
