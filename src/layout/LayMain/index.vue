b2e62f3fb158ed506e57027702a8d6da49cf3446
hello <zhizous@163.com>
2021-07-25 23:03:43 +0800

    :construction: 刷新问题

--- src/layout/LayMain/index.vue
+++ src/layout/LayMain/index.vue
@@ -3,42 +3,40 @@
 -->
 
 <template>
+  <n-button @click="handleReload">点击</n-button>
   <router-view v-slot="{ Component, route }">
     <transition appear :name="route.meta.transition || 'fade'" mode="out-in">
       <keep-alive v-if="keepAlives" :include="keepAlives">
-        <component :key="route.fullPath" :is="Component" />
+        <component v-if="reload" :key="route.fullPath" :is="Component" />
       </keep-alive>
-      <component v-else :is="Component" :key="route.fullPath" />
+      <component v-else-if="reload" :is="Component" :key="route.fullPath" />
     </transition>
   </router-view>
 </template>
 <script lang="ts">
 import { useStore } from 'vuex';
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
   name: 'layMain',
   setup() {
+    const reload = ref(true);
     const store = useStore();
     const keepAlives = store.getters['admin/keepAlivesGetter'];
-    /* 
-      watch(
-        route,
-        () => {
-          if (route.meta && route.meta.keepAlive) {
-            store.commit(
-              'admin/ADD_KEEPALIVES',
-              route.matched[route.matched.length - 1].components.default.name
-            );
-          }
-        },
-        {
-          immediate: true
-        }
-      );
-    */
-    return { keepAlives };
+
+    return {
+      keepAlives, reload, handleReload() {
+        const a = keepAlives[0]
+        store.commit('admin/DEL_KEEPALIVES', a);
+        reload.value = false;
+        setTimeout(() => {
+          store.commit('admin/ADD_KEEPALIVES', a);
+          reload.value = true;
+        }, 150)
+      }
+    };
   }
 });
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+</style>
