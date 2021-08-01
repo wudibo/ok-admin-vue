64a179558738ae612867bf7873ce0ae3cb25f972
hello <zhizous@163.com>
2021-08-01 20:52:22 +0800

    :heavy_plus_sign: 设置暗夜主题

---
+++ src/layout/LaySetting/components/SetDark.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="dark-box">
+    <n-switch :value="isDarkTheme" @update:value="handleDarkTheme">
+      <template #checked>
+        <n-icon>
+          <moon />
+        </n-icon>
+      </template>
+      <template #unchecked>
+        <n-icon>
+          <sunny />
+        </n-icon>
+      </template>
+    </n-switch>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import { useStore } from "vuex";
+import { NSwitch, NIcon } from "naive-ui";
+import { Sunny, Moon } from "@vicons/ionicons5";
+export default defineComponent({
+  name: "SetDark",
+  components: { NSwitch, NIcon, Sunny, Moon },
+  setup() {
+    const store = useStore();
+    const isDarkTheme = ref(store.getters['theme/isDarkThemeGetter']);
+
+    return {
+      isDarkTheme,
+      handleDarkTheme(val: boolean) {
+        isDarkTheme.value = val
+        store.commit('theme/SET_ISDARKTHEME', val)
+      }
+    }
+  }
+})
+</script>
+
+<style lang="scss">
+.dark-box {
+  text-align: center;
+}
+</style>
