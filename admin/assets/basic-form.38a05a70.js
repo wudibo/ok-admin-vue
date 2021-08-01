d6c7081e89086a004c5cfef4235329fd08493291
hello <zhizous@163.com>
2021-08-01 20:56:49 +0800

    :package: 打包

--- admin/assets/basic-form.38a05a70.js
+++
@@ -1,1 +0,0 @@
-import{d as r,e as a,f as e}from"./vendor.221c57bb.js";var n=r({name:"basic-form"});const o={class:"app-container"};n.render=function(r,n,s,c,t,f){return a(),e("div",o,"basic-form")};export default n;
