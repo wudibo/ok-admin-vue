ed08a5b74415d84bf7fc79f09c776c8160d3dfd9
jianglong <zhizous@163.com>
2022-02-07 17:37:28 +0800

    配置jest

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -5,7 +5,7 @@ describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message'
     const wrapper = shallowMount(HelloWorld as any, {
-      props: { msg: { dsafasdf: 4 } }
+      props: { msg }
     })
     expect(wrapper.text()).toMatch(msg)
   })
