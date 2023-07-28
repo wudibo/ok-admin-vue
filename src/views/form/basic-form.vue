dff272459754d5e98dbbe7ecef7e9efd55aeb2ac
jianglong <zhizous@163.com>
2023-07-28 18:27:58 +0800

    🌈 style: 富文本

--- src/views/form/basic-form.vue
+++ src/views/form/basic-form.vue
@@ -1,12 +1,31 @@
 <template>
-  <div class="app-container">basic-form</div>
+  <div class="app-container">
+    <div class="title">输入富文本可及时显示</div>
+    <n-input v-model:value="value" type="textarea" rows="8" placeholder="基本的 Textarea" />
+    <div class="html">
+      <div v-html="value"></div>
+    </div>
+  </div>
 </template>
 
-<script lang="ts">
-  import { defineComponent } from 'vue'
-  export default defineComponent({
-    name: 'BasicForm'
-  })
+<script lang="ts" setup>
+  import { ref } from 'vue'
+  const value = ref(`
+    <img
+      loading="eager"
+      width="240"
+      src="https://foruda.gitee.com/avatar/1669426812317011153/1326874_zhizous_1669426812.png"
+    />
+  `)
 </script>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+  .title {
+    font-size: 18px;
+  }
+  .html {
+    margin-top: 20px;
+    border: 1px solid #ccc;
+    padding: 10px;
+  }
+</style>
