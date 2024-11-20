d15fbf5c51e59c4dc28f67e0dd87f1e3fa58646d
jianglong <jianglong@yupaowang.com>
2024-11-20 17:01:51 +0800

    🎈 perf: 符文本

--- src/views/form/basic-form.vue
+++ src/views/form/basic-form.vue
@@ -1,12 +1,45 @@
 <template>
-  <div class="app-container">basic-form</div>
+  <div class="app-container">
+    <n-card style="margin-bottom: 20px;" :bordered="false" title="回显示符文本">
+      <div class="flex-col" v-html="html"></div>
+    </n-card>
+    <n-card :bordered="false" title="输入符文本">
+      <n-input
+        v-model:value="html"
+        type="textarea"
+        placeholder="基本的 Textarea"
+        :autosize="{
+          minRows: 7,
+          maxRows: 50,
+        }"
+      />
+    </n-card>
+  </div>
 </template>
-
 <script lang="ts">
-  import { defineComponent } from 'vue'
-  export default defineComponent({
-    name: 'BasicForm'
-  })
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'BasicForm'
+})
 </script>
 
-<style lang="scss"></style>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const html = ref(
+`<div>
+  <img src="https://foruda.gitee.com/avatar/1669426812317011153/1326874_zhizous_1669426812.png" />
+</div>
+<div>
+  <img src= />
+</div>`
+)
+
+</script>
+
+<style lang="scss">
+.flex-col {
+  display: flex;
+  flex-direction: column;
+}
+</style>
